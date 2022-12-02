e4fdc2c2880fadf908819f977d9f9e5f47e0b530
DevinForr <117605849+DevinForr@users.noreply.github.com>
2022-12-02 09:15:08 -0500

    Added Day 2 Code

---
+++ part2.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int result(char elf, char you) {
+  int score = 0;
+  int win = 6;
+  int draw = 3;
+  int rock = 1, paper = 2, scissors = 3;
+  if (elf == 'A') {
+    if (you == 'X') {
+      score += scissors;
+    } else if (you == 'Y') {
+      score += rock+draw;
+    } else if (you == 'Z') {
+      score += paper+win;
+    }
+  } else if (elf == 'B') {
+    if (you == 'X') {
+      score += rock;
+    } else if (you == 'Y') {
+      score += paper+draw;
+    } else if (you == 'Z') {
+      score += scissors+win;
+    }
+  } else if (elf == 'C') {
+    if (you == 'X') {
+      score += paper;
+    } else if (you == 'Y') {
+      score += scissors+draw;
+    } else if (you == 'Z') {
+      score += rock+win;
+    }
+  }
+  return score;
+}
+
+int main(void) {
+  FILE *fp;
+  fp = fopen("input.txt", "r");
+  ssize_t read;
+  size_t length = 10;
+  char *line = malloc(10);
+  int score = 0;
+  char you, elf;
+  while ((read = getline(&line, &length, fp)) != -1) {
+     sscanf(line, "%c %c", &elf, &you);
+     score += result(elf, you);
+  }
+  printf("Total Score: %d", score);
+  free(line);
+  return 0;
+} 
